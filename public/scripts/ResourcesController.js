36a2b46f0136a16fdaad57e94d07589e55164e5c
Shaina Karasin <skarasin39@gmail.com>
2016-10-10 13:48:13 -0400

    normalize.css & jQ data filter

--- public/scripts/ResourcesController.js
+++ public/scripts/ResourcesController.js
@@ -1,6 +1,22 @@
 (function() {
     function ResourcesCtrl ($scope, Fixtures) {
-        // logic
+        $scope.filterClicked = function(event) {
+            console.log(event);
+            var $chosenFilter = $(event.target);
+            var chosenClass = event.target.classList[1];
+            
+            // change active filter
+            $('.filter').removeClass('active');
+            $chosenFilter.addClass('active');
+            
+            // filter items
+            if (chosenClass == 'all') {
+                $('#dataHolder').children('div.item').show();
+            } else {
+                $('#dataHolder').children('div:not(.'+chosenClass+')').hide();
+                $('#dataHolder').children('div.'+chosenClass).show();
+            }
+        }
     }
 
     angular
--- public/scripts/SearchController.js
+++ public/scripts/SearchController.js
@@ -1,20 +1,27 @@
 (function() {
     function SearchCtrl ($scope, Fixtures, $http) {
         
-        /*  $scope.mySearch includes:
-            .geography
-            .activity
-            .language ~uppercased
-            .price
-            .size
-            .climate
-        */
+/*  $scope.mySearch can include:
+    .geography
+    [North America, South America, Central America, Europe, Africa, Asia, Australia]
+    .activity
+    [Nightlife, Nature]
+    .language
+    [English, Spanish, French, Arabic]
+    .price
+    [Budget, Moderate, Luxury]
+    .size
+    [Rural, Small City, Major City]
+    .climate
+    [Desert, Mountains, Rainforest, Temperate]
+*/
         
         $scope.search = function() {
             $scope.results = [];
             $scope.showResults = false;
             if ($scope.mySearch !== undefined) {
-                $http.get('/search', {params: $scope.mySearch})
+                $scope.mySearch["geography"] = {$in: ["North America", "South America"]};
+                $http.post('/search', $scope.mySearch)
                      .then(
                         function(response) {
                             console.log('mySearch: ',$scope.mySearch);
@@ -28,6 +35,10 @@
             } else {
                 console.log('search undefined');
             }
+        }
+        
+        $scope.reset = function() {
+            $scope.mySearch = {};
         }
     }
 

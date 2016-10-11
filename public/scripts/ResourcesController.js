fc79a8f19fc4b0c14abec81b7cfdcc060ba15d48
Shaina Karasin <skarasin39@gmail.com>
2016-10-10 22:32:12 -0400

    css for resources page

--- public/scripts/ResourcesController.js
+++ public/scripts/ResourcesController.js
@@ -1,14 +1,12 @@
 (function() {
-    function ResourcesCtrl ($scope, Fixtures) {
+    function ResourcesCtrl ($scope, $rootScope, Fixtures) {
         $scope.filterClicked = function(event) {
             console.log(event);
             var $chosenFilter = $(event.target);
             var chosenClass = event.target.classList[1];
-            
             // change active filter
             $('.filter').removeClass('active');
             $chosenFilter.addClass('active');
-            
             // filter items
             if (chosenClass == 'all') {
                 $('#dataHolder').children('div.item').show();
@@ -17,6 +15,9 @@
                 $('#dataHolder').children('div.'+chosenClass).show();
             }
         }
+        
+        $scope.itemTitle = 'Item Title';
+        $scope.itemDescr = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate'
     }
 
     angular

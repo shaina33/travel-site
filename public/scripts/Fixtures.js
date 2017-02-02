653e6d50805890a2ef782c699093811a2bf556c3
Shaina Karasin <skarasin39@gmail.com>
2017-02-02 15:28:39 -0500

    resources dynamic from Fixtures & alphabetized

--- public/scripts/Fixtures.js
+++ public/scripts/Fixtures.js
@@ -21,6 +21,32 @@
             }
         ]
         
+        // placeholder resource description
+        var resDesc = 'Placeholder - Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate';
+        
+        /*
+        category options:
+            cat-activ >> Activities
+            cat-trans >> Transportation
+            cat-lodg >> Lodging
+            cat-inv >> Get Involved
+            cat-other >> Other
+        */      
+        Fixtures.resources = [
+            tripadvisor = {
+                itemTitle: 'Trip Advisor',
+                category: 'cat-activ',
+                webLink: 'http://www.tripadvisor.com',
+                itemDescr: resDesc
+            },
+            kayak = {
+                itemTitle: 'Kayak',
+                category: 'cat-trans',
+                webLink: 'http://www.kayak.com',
+                itemDescr: resDesc
+            }
+        ]
+        
         return Fixtures;
     }
 
--- public/scripts/ResourcesController.js
+++ public/scripts/ResourcesController.js
@@ -16,11 +16,8 @@
             }
         }
         
-        // Placeholder Content //
-        $scope.itemTitle = 'Item Title';
-        $scope.category = 'cat-activ';
-        $scope.webLink;
-        $scope.itemDescr = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate'
+        $scope.resources = Fixtures.resources;
+        
     }
 
     angular
